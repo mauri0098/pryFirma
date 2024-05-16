317b620ee0955a3a49e1c1e0784d48c16a810548
mauri0098 <maurirodriguez0098@gmail.com>
2024-05-16 08:44:10 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace pryFirma
+{
+    partial class frmFirma
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pbFirma = new PictureBox();
+            btnGuardar = new Button();
+            rdBlue = new RadioButton();
+            rdRed = new RadioButton();
+            rdOrange = new RadioButton();
+            gbColors = new GroupBox();
+            btnLimpiar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pbFirma).BeginInit();
+            gbColors.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pbFirma
+            // 
+            pbFirma.Location = new Point(1, 3);
+            pbFirma.Name = "pbFirma";
+            pbFirma.Size = new Size(576, 359);
+            pbFirma.TabIndex = 0;
+            pbFirma.TabStop = false;
+            pbFirma.MouseMove += pbFirma_MouseMove;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(583, 161);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(81, 34);
+            btnGuardar.TabIndex = 1;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // rdBlue
+            // 
+            rdBlue.AutoSize = true;
+            rdBlue.Checked = true;
+            rdBlue.Location = new Point(6, 20);
+            rdBlue.Name = "rdBlue";
+            rdBlue.Size = new Size(48, 19);
+            rdBlue.TabIndex = 2;
+            rdBlue.TabStop = true;
+            rdBlue.Text = "Blue";
+            rdBlue.UseVisualStyleBackColor = true;
+            // 
+            // rdRed
+            // 
+            rdRed.AutoSize = true;
+            rdRed.Location = new Point(6, 45);
+            rdRed.Name = "rdRed";
+            rdRed.Size = new Size(45, 19);
+            rdRed.TabIndex = 3;
+            rdRed.Text = "Red";
+            rdRed.UseVisualStyleBackColor = true;
+            // 
+            // rdOrange
+            // 
+            rdOrange.AutoSize = true;
+            rdOrange.Location = new Point(6, 70);
+            rdOrange.Name = "rdOrange";
+            rdOrange.Size = new Size(64, 19);
+            rdOrange.TabIndex = 4;
+            rdOrange.TabStop = true;
+            rdOrange.Text = "Orange";
+            rdOrange.UseVisualStyleBackColor = true;
+            // 
+            // gbColors
+            // 
+            gbColors.Controls.Add(rdOrange);
+            gbColors.Controls.Add(rdBlue);
+            gbColors.Controls.Add(rdRed);
+            gbColors.Location = new Point(580, 3);
+            gbColors.Name = "gbColors";
+            gbColors.Size = new Size(84, 110);
+            gbColors.TabIndex = 5;
+            gbColors.TabStop = false;
+            gbColors.Text = "Colors";
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(583, 119);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(81, 36);
+            btnLimpiar.TabIndex = 6;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // frmFirma
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(676, 364);
+            Controls.Add(btnLimpiar);
+            Controls.Add(gbColors);
+            Controls.Add(btnGuardar);
+            Controls.Add(pbFirma);
+            Name = "frmFirma";
+            Text = "Firma";
+            ((System.ComponentModel.ISupportInitialize)pbFirma).EndInit();
+            gbColors.ResumeLayout(false);
+            gbColors.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pbFirma;
+        private Button btnGuardar;
+        private RadioButton rdBlue;
+        private RadioButton rdRed;
+        private RadioButton rdOrange;
+        private GroupBox gbColors;
+        private Button btnLimpiar;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+using System.Windows.Forms;
+
+namespace pryFirma
+{
+    public partial class frmFirma : Form
+    {
+        private Bitmap imagen;
+        public frmFirma()
+        {
+            InitializeComponent();
+            imagen = new Bitmap(pbFirma.Width, pbFirma.Height);
+        }
+
+        private void pbFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && rdBlue.Checked == true)
+            {
+                using (Graphics objetoLoco = Graphics.FromImage(imagen))
+                {
+                    objetoLoco.FillEllipse(Brushes.Blue, e.X, e.Y, 5, 5);
+                }
+                pbFirma.Image = imagen;
+            }
+            else if (e.Button == MouseButtons.Left && rdRed.Checked == true)
+            {
+                using (Graphics objetoLoco = Graphics.FromImage(imagen))
+                {
+                    objetoLoco.FillEllipse(Brushes.Red, e.X, e.Y, 5, 5);
+                }
+                pbFirma.Image = imagen;
+            }
+            else if (e.Button == MouseButtons.Left && rdOrange.Checked == true)
+            {
+                using (Graphics objetoLoco = Graphics.FromImage(imagen))
+                {
+                    objetoLoco.FillEllipse(Brushes.Orange, e.X, e.Y, 5, 5);
+                }
+                pbFirma.Image = imagen;
+            }
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            imagen = new Bitmap(pbFirma.Width, pbFirma.Height); // Crea una nueva imagen en blanco
+            pbFirma.Image = imagen; // Asigna la nueva imagen al PictureBox
+        }
+    }
+}
